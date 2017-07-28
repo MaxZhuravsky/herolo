cc1b22c426b8dd6ab20b9f6874d7bfd33566e0a2
Max Zhuravsky <maxzhuravsky2@gmail.com>
2017-07-28 20:33:36 +0300

    added links to pictures

--- src/app/models/Book.ts
+++ src/app/models/Book.ts
@@ -2,10 +2,12 @@ export class Book {
   title: string;
   date: Date;
   author: string;
+  link: string;
 
-  constructor(title: string, date: Date, author: string) {
+  constructor(title: string, date: Date, author: string, link: string) {
     this.title = title;
     this.date = date;
     this.author = author;
+    this.link = link;
   }
 }
